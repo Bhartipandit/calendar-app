ffd49b5e3431644045aac0797fcb106e7f4e663b
Bharti <bhartipandit098@gmail.com>
2025-12-24 07:01:09 +0530

    main privacy policy

---
+++ src/app/privacy-policy/privacy-policy.js
@@ -0,0 +1,19 @@
+export default privacyPolicyPage = () => {
+  return (
+    <>
+      <p>Privacy Policy This application uses Google OAuth to access Google
+      Calendar data. 
+
+      We only access data necessary to create, read, update, or delete calendar events explicitly requested by the user.
+
+      We do not store, share, or sell user data to third parties.
+
+      All authentication is handled securely via Google OAuth. 
+      No Google account passwords are stored.
+
+      If you have any questions, contact us at: 
+      hello.bhartipandit@gmail.com
+      </p>
+    </>
+  );
+};
---
+++ src/app/terms/terms.js
@@ -0,0 +1,18 @@
+export default termsPage = () => {
+  return (
+    <>
+      <p>Terms of Service
+
+        By using this application, you agree to allow access to your Google Calendar
+        only for the features explicitly provided by the app.
+
+        This app is provided "as is" without warranties of any kind.
+
+        We are not responsible for data loss, calendar misconfigurations,
+        or service interruptions.
+
+        You may revoke access at any time from your Google Account settings.
+      </p>
+    </>
+  );
+};
